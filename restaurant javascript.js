4f841d17cf4d3cc18fbb1ec2e18d298de65a43fc
Lilian <lilian.ade@hotmail.co.uk>
2019-12-16 19:19:09 +0000

    tt+++++++++++++++++++++++++++++++++++++++++++++++++++

--- restaurant javascript.js
+++ restaurant javascript.js
@@ -1,1 +1,18 @@
-Bootstrap JS
+$(".row").each(function(){
+    var width = $(this).find("img").width();
+    $(".overlay").width(width);
+    var height = $(this).find("img").height();
+    $(".overlay").height(height);
+});
+$( window ).resize(function() {
+    $(".row").each(function(){
+        var width = $(this).find("img").width();
+        $(".overlay").width(width);
+        var height = $(this).find("img").height();
+        $(".overlay").height(height);
+    });
+});
+ $( "logo" ).hover(function() {
+ $( this ).fadeOut( 1000 );
+ $( this ).fadeIn( 1000 );
+})
